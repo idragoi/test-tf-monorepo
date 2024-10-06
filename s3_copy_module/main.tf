af30177a93848dc0bc006f92eb9e2c43b22594a0
idragoi <john.dragoi87@gmail.com>
2024-10-06 17:13:10 +0300

    format tf

--- s3_copy_module/main.tf
+++ s3_copy_module/main.tf
@@ -4,21 +4,21 @@ module "target_s3_bucket" {
 
   bucket_prefix = "${var.module_name}-bucket-"
   attach_policy = true
-  policy = data.aws_iam_policy_document.bucket_policy.json
+  policy        = data.aws_iam_policy_document.bucket_policy.json
 
-  attach_public_policy = false
+  attach_public_policy                     = false
   attach_deny_insecure_transport_policy    = true
   attach_require_latest_tls_policy         = true
   attach_deny_incorrect_encryption_headers = true
   attach_deny_unencrypted_object_uploads   = true
 
   restrict_public_buckets = true
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
-  
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+
   server_side_encryption_configuration = var.encryption_config
-  versioning = var.versioning_config
+  versioning                           = var.versioning_config
 }
 
 data "aws_iam_policy_document" "bucket_policy" {
@@ -41,13 +41,13 @@ module "eventbridge_lambda_schedule" {
   version = "3.11.0"
 
   create_bus = false
-  role_name = "${var.module_name}-eb-role"
+  role_name  = "${var.module_name}-eb-role"
 
   rules = {
     cron_daily = {
       description         = var.description
       schedule_expression = var.copy_schedule
-      state = var.environment == "prod" ? "ENABLED" : "DISABLED"
+      state               = var.environment == "prod" ? "ENABLED" : "DISABLED"
     }
   }
 
@@ -66,19 +66,19 @@ module "lambda_function" {
   source  = "terraform-aws-modules/lambda/aws"
   version = "7.9.0"
 
-  function_name          = "${var.module_name}-function"
-  description            = var.description
-  handler                = "lambda_function.lambda_handler"
-  runtime                = "python3.12"
-  memory_size            = var.lambda_function_memory_size
-  ephemeral_storage_size = var.lambda_function_storage_size
-  vpc_subnet_ids         = var.lambda_function_vpc_subnet_list
-  vpc_security_group_ids = var.lambda_function_security_group_list
-  architectures          = ["arm64"]
-  publish                = false
+  function_name              = "${var.module_name}-function"
+  description                = var.description
+  handler                    = "lambda_function.lambda_handler"
+  runtime                    = "python3.12"
+  memory_size                = var.lambda_function_memory_size
+  ephemeral_storage_size     = var.lambda_function_storage_size
+  vpc_subnet_ids             = var.lambda_function_vpc_subnet_list
+  vpc_security_group_ids     = var.lambda_function_security_group_list
+  architectures              = ["arm64"]
+  publish                    = false
   create_lambda_function_url = false
-  attach_network_policy  = true
-  attach_dead_letter_policy = false
+  attach_network_policy      = true
+  attach_dead_letter_policy  = false
 
   # package_type = "Zip"
   # local_existing_package = var.file_name_zip
@@ -90,13 +90,13 @@ module "lambda_function" {
   timeout = 900
 
   environment_variables = {
-    LOGGING_LEVEL      = var.lambda_logging_level
-    S3_BASE_PREFIX     = var.source_bucket_log_prefix
+    LOGGING_LEVEL        = var.lambda_logging_level
+    S3_BASE_PREFIX       = var.source_bucket_log_prefix
     LAST_COPY_PARAM_NAME = module.ssm_parameter.ssm_parameter_name
-    SOURCE_BUCKET      = var.source_bucket_name
-    TARGET_BUCKET      = module.target_s3_bucket.s3_bucket_id
-    COPY_LOGS_ROLE_ARN = var.copy_logs_role_arn
-    MODULE_NAME        = var.module_name
+    SOURCE_BUCKET        = var.source_bucket_name
+    TARGET_BUCKET        = module.target_s3_bucket.s3_bucket_id
+    COPY_LOGS_ROLE_ARN   = var.copy_logs_role_arn
+    MODULE_NAME          = var.module_name
   }
 
   allowed_triggers = {
@@ -107,7 +107,7 @@ module "lambda_function" {
   }
   create_current_version_allowed_triggers = false
 
-  cloudwatch_logs_log_group_class = "STANDARD"
+  cloudwatch_logs_log_group_class   = "STANDARD"
   cloudwatch_logs_retention_in_days = 30
 
   role_path   = "/tf-managed/"
@@ -121,11 +121,11 @@ module "lambda_function" {
       resources = [var.copy_logs_role_arn]
     }
     ssm_param = {
-      effect    = "Allow",
-      actions   = [
+      effect = "Allow",
+      actions = [
         "ssm:GetParameter",
         "ssm:PutParameter"
-        ],
+      ],
       resources = [module.ssm_parameter.ssm_parameter_arn]
     }
   }
@@ -135,10 +135,10 @@ module "ssm_parameter" {
   source  = "terraform-aws-modules/ssm-parameter/aws"
   version = "1.1.1"
 
-  name            = "${var.module_name}-param"
-  description     = var.description
-  data_type       = "text"
-  tier            = "Standard"
-  value           = var.copy_objects_start_date
+  name                 = "${var.module_name}-param"
+  description          = var.description
+  data_type            = "text"
+  tier                 = "Standard"
+  value                = var.copy_objects_start_date
   ignore_value_changes = true
 }
